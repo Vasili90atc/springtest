cdf0694c00a07742f080070225c3067f70e7c346
IVasili <IVasili@ds-ivasili.atc.gr>
2023-10-31 16:46:35 +0200

    spring-test

---
+++ src/test/java/gr/atc/training/spring/test/SpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.atc.training.spring.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
